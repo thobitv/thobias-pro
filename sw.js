8cc42306a2c2e23375c24eb89be27fd9fe573fbc
thobitv <thobiastavares@gmail.com>
2026-02-20 14:37:29 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const V = 'tp-51b';
+const ASSETS = ['./', './thobias_pro_v51.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(V).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== V).map(k => caches.delete(k)))
+  ).then(() => self.clients.claim()));
+});
+
+// Network-first: sempre busca versão nova online; só usa cache se offline
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).then(resp => {
+      const clone = resp.clone();
+      caches.open(V).then(c => c.put(e.request, clone));
+      return resp;
+    }).catch(() => caches.match(e.request))
+  );
+});
