4a2ccccedf15b02297a4a7b1e49171e21530cf91
thobitv <thobiastavares@gmail.com>
2026-02-24 08:22:33 -0300

    Update cache version from 'tp-53' to 'tp-53a'

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V = 'tp-53';
+const V = 'tp-53a';
 const ASSETS = ['./', './thobias_pro_v51.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
