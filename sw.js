5cc1f5c475a60a80868c490932e397dd11d0d3f1
thobitv <thobiastavares@gmail.com>
2026-02-24 08:38:21 -0300

    Update service worker version from tp-53a to tp-54

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V = 'tp-53a';
+const V = 'tp-54';
 const ASSETS = ['./', './thobias_pro_v51.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
