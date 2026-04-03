784e6cd46769819e7d6b74a7e2e7dd9f1f043c38
thobitv <thobiastavares@gmail.com>
2026-04-02 21:27:27 -0300

    Update version from 'tp-60' to 'tp-61'

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V = 'tp-60';
+const V = 'tp-61';
 const ASSETS = ['./', './thobias_pro_v51.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
