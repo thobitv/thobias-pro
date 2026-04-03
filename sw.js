35e5b10fc471959fe61514044e723789013ff80b
thobitv <thobiastavares@gmail.com>
2026-04-03 10:59:24 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V = 'tp-61';
+const V = 'tp-61a';
 const ASSETS = ['./', './thobias_pro_v51.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
