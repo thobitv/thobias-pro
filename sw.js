1e897c979b49c4054491f8768a121ab67480a5e2
thobitv <thobiastavares@gmail.com>
2026-04-28 10:22:00 -0300

    Update service worker version from tp-61b to tp-62

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V = 'tp-61b';
+const V = 'tp-62';
 const ASSETS = ['./', './thobias_pro_v51.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
