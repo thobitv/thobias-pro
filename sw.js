490bc68f489eb5ed40f4dbe7d5fcf532c11fb5e4
thobitv <thobiastavares@gmail.com>
2026-05-04 06:10:25 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V = 'tp-62.4';
+const V = 'tp-63';
 const ASSETS = ['./', './thobias_pro_v51.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
