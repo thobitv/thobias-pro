026f5a0abbfa02d3c413ed203ec9b557a514faaa
thobitv <thobiastavares@gmail.com>
2026-05-26 08:50:52 -0300

    Refactor service worker event listeners into single lines

--- sw.js
+++ sw.js
@@ -1,22 +1,5 @@
-const V = 'tp-64e';
+const V = 'tp-64g';
 const ASSETS = ['./', './thobias_pro_v51.html', './manifest.json', './icon-192.png', './icon-512.png'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(V).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== V).map(k => caches.delete(k)))
-  ).then(() => self.clients.claim()));
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).then(resp => {
-      const clone = resp.clone();
-      caches.open(V).then(c => c.put(e.request, clone));
-      return resp;
-    }).catch(() => caches.match(e.request))
-  );
-});
+self.addEventListener('install', e => { e.waitUntil(caches.open(V).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== V).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
+self.addEventListener('fetch', e => { e.respondWith(fetch(e.request).then(resp => { const clone = resp.clone(); caches.open(V).then(c => c.put(e.request, clone)); return resp; }).catch(() => caches.match(e.request))); });
